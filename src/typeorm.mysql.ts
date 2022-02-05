6fd87a005b6b94024286549d6c9fe744b5afdfe8
dangkien <dangkien@gmail.com>
2022-02-05 21:31:18 +0700

    f

--- src/typeorm.mysql.ts
+++ src/typeorm.mysql.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres' ,
   host: 'ec2-18-235-114-62.compute-1.amazonaws.com',
-  port: 5432 || 3306,
-  username: 'mkfjmyrvsgdlvh' || 'postgres',
-  password: '6d14523586df2bc08a469a42525fea23a303f3e9fc1a6b6688d5c91d7503ac19' || 'root',
-  database: 'd56341gdrl2kpf' || 'chat_socket',
+  port: 5432 ,
+  username: 'mkfjmyrvsgdlvh' ,
+  password: '6d14523586df2bc08a469a42525fea23a303f3e9fc1a6b6688d5c91d7503ac19' ,
+  database: 'd56341gdrl2kpf' ,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
